012cf2bf6f85af1f05878ae6355a0bf7007f144f
Sebastian Zaruha <sebastianzaruha@gmail.com>
2024-11-18 17:12:13 +0100

    .

--- src/main/java/com/cesur/splinterio/repositories/CommentRepository.java
+++ src/main/java/com/cesur/splinterio/repositories/CommentRepository.java
@@ -3,8 +3,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cesur.splinterio.models.Comment;
+import com.cesur.splinterio.models.Incidence;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>{
+
+    Comment[] findAllByIncidence(Incidence incidence);
 
 
 
